aeed5e0a4dcd55cf2f9b1ed995a4df35ad2ab489
Matthew Coatney <mcoatney@bitwiseindustries.com>
2023-02-13 12:16:38 -0800

    chore(app-routing.module): Adds routes for demo components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { ChatComponent } from './chat/chat.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'chat', component: ChatComponent },
+  { path: '', component: AuthComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
